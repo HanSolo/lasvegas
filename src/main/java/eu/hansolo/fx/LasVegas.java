4006ed6005568b3749020908d3c6c33bf7da8850
Gerrit Grunwald <han.solo@mac.com>
2022-10-17 10:44:32 -0700

    Initial code commit

--- src/main/java/eu/hansolo/fx/LasVegas.java
+++ src/main/java/eu/hansolo/fx/LasVegas.java
@@ -1,50 +1,67 @@
+ /*
+  * Copyright (c) 2022 by Gerrit Grunwald
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
  package eu.hansolo.fx;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+
+ import javafx.application.Application;
+ import javafx.application.Platform;
+ import javafx.geometry.Insets;
+ import javafx.scene.Scene;
+ import javafx.scene.layout.StackPane;
+ import javafx.stage.Stage;
 
 
-/**
- * User: hansolo
- * Date: 17.10.22
- * Time: 10:34
- */
-public class LasVegas extends Application {
-    
-    @Override public void init() {
-    
-    }
-    
-    private void initOnFxApplicationThread(final Stage stage) {
-    
-        registerListeners();
-    }
-    
-    private void registerListeners() {
-    
-    }
-    
-    @Override public void start(final Stage stage) {
-        StackPane pane = new StackPane();
-        pane.setPadding(new Insets(10));
-        
-        Scene scene = new Scene(pane);
-        
-        stage.setTitle("Title");
-        stage.setScene(scene);
-        stage.show();
-        stage.centerOnScreen();
-    }
-    
-    @Override public void stop() {
-        Platform.exit();
-        System.exit(0);
-    }
-    
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+ /**
+  * User: hansolo
+  * Date: 17.10.22
+  * Time: 10:34
+  */
+ public class LasVegas extends Application {
+
+     @Override public void init() {
+
+     }
+
+     private void initOnFxApplicationThread(final Stage stage) {
+
+         registerListeners();
+     }
+
+     private void registerListeners() {
+
+     }
+
+     @Override public void start(final Stage stage) {
+         StackPane pane = new StackPane();
+         pane.setPadding(new Insets(10));
+
+         Scene scene = new Scene(pane);
+
+         stage.setTitle("Title");
+         stage.setScene(scene);
+         stage.show();
+         stage.centerOnScreen();
+     }
+
+     @Override public void stop() {
+         Platform.exit();
+         System.exit(0);
+     }
+
+     public static void main(String[] args) {
+         launch(args);
+     }
+ }
--- src/main/java/eu/hansolo/fx/fonts/Fonts.java
+++ src/main/java/eu/hansolo/fx/fonts/Fonts.java
@@ -1,2 +1,38 @@
-package eu.hansolo.fx.fonts;public class Fonts {
+/*
+ * Copyright (c) 2022 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.fx.fonts;
+
+import javafx.scene.text.Font;
+
+
+public class Fonts {
+    private static final String NEUTON_700_NAME;
+
+    private static String neuton700Name;
+
+
+    static {
+        try {
+            neuton700Name = Font.loadFont(Fonts.class.getResourceAsStream("/eu/hansolo/fx/lasvegas/fonts/neuton-v18-700.ttf"), 10).getName();
+        } catch (Exception exception) { }
+        NEUTON_700_NAME = neuton700Name;
+    }
+
+
+    // ******************** Methods *******************************************
+    public static Font neuton700(final double size) { return new Font(NEUTON_700_NAME, size); }
 }
